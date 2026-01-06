c98ba8a6bf9f49aa4061fb7296d7d063c6499ff7
Sam Foster <sam@sam-i-am.com>
2026-01-05 23:38:35 -0800

    Fix/undo text-on-qrcode stuff so it just creates and shows a qrcode image from a URL

--- server/make_qrcode.py
+++ server/make_qrcode.py
@@ -8,38 +8,13 @@ qr = qrcode.QRCode(
     box_size=10,  # size of each box in pixels
     border=4  # border thickness in boxes
 )
-qr.add_data('https://example.com')
+qr.add_data('https://euglink.org/ZnmcSq')
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
-width, height = img.size
-
-# get a font
-fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
-
-# make a blank image for the text, initialized to transparent text color
-# txt = Image.new("RGBA", img.size, (255, 255, 255, 255))
-
-# get a drawing context
-draw = ImageDraw.Draw(img)
-
-# draw text, half opacity
-label_text = "Hello World"
-textwidth, textheight = draw.textsize(label_text, font=fnt)
-
-print(f'width {width}', width)
-x = (width - textwidth) / 2
-y = (height - textheight) / 2
-
-backfill = ImageDraw.Draw(img)
-backfill.rectangle([(x,y), (x+textwidth, y+textheight)], fill="magenta", outline=None, width=1)
-
-# Add text on top
-draw.text((x, y), label_text, font=fnt, fill="black")
-
 
 # Save the result
-img.save('qr_with_text.png')
+img.save('qr_code_image.png')
 
 # Show the image
 img.show()
